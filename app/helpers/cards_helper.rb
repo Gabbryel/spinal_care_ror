4d87bf60cda8a1c1beee69cb0145100b5eba00b6
Gabriel Ursache <gabriel@markov.ro>
2022-08-21 12:10:41 +0300

    stimulus connected

---
+++ app/helpers/cards_helper.rb
@@ -0,0 +1,30 @@
+module CardsHelper
+  def cards
+    cards = [
+      {
+        icon: '<i class="fas fa-hand-holding-usd"></i>',
+        title: 'Costuri',
+        content: 'Avem prețuri competitive și contract cu Casa de Asigurări',
+        link: '#',
+      },
+      {
+        icon: '<i class="fas fa-hand-holding-usd"></i>',
+        title: 'Strategie',
+        content: 'Urmărim cu atenție inovația din domeniu și achiziționăm aparatură performantă',
+        link: '#',
+      },
+      {
+        icon: '<i class="fas fa-university"></i>',
+        title: 'Pregătire',
+        content: 'Credem în principiul studiului pe tot parcursul carierei.',
+        link: '#',
+      },
+      {
+        icon: '<i class="fas fa-hand-holding-usd"></i>',
+        title: 'Contact',
+        content: 'Vă stăm la dispoziție pentru programări, sfaturi și susținere',
+        link: '#',
+      },
+    ]
+  end
+end
---
+++ app/javascript/controllers/vh_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus";
+import { useWindowResize } from 'stimulus-use';
+
+export default class extends Controller {
+  static targets = ['height']
+
+  connect() {
+    useWindowResize(this)
+    document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+  }
+  windowResize({ width, height, event }) {
+    console.log(height)
+    document.documentElement.style.setProperty("--vh", `${height * 0.01}px`);
+  }
+}
