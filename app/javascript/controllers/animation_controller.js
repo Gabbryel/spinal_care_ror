22ba7525cff6c5ade059d04b81bb28f8773f83d1
Gabriel Ursache <gabriel@markov.ro>
2022-08-22 11:54:25 +0300

    animations main page done

--- app/javascript/controllers/animation_controller.js
+++ app/javascript/controllers/animation_controller.js
@@ -3,14 +3,19 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   svgload() {
     this.element.classList.add('active-svg')
+    this.element.classList.remove('inactive')
   }
   svgscroll() {
     if (window.innerHeight < this.element.getBoundingClientRect().top) {
       this.element.classList.remove('active-svg')
+      this.element.classList.add('inactive')
     } else if (window.innerHeight > this.element.getBoundingClientRect().top && this.element.getBoundingClientRect().bottom > 0) {
+      this.element.classList.add('trigger')
       this.element.classList.add('active-svg')
+      this.element.classList.remove('inactive')
     } else if (this.element.getBoundingClientRect().bottom < 0) {
       this.element.classList.remove('active-svg')
+      this.element.classList.add('inactive')
     }
   }
   }
