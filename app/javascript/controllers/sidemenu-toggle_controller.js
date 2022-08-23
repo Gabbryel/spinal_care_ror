66c105a454dd00a675ab3f14725cfa2ff416b3fd
Gabriel Ursache <gabriel@markov.ro>
2022-08-23 08:06:06 +0300

    navbar anim done

--- app/javascript/controllers/sidemenu-toggle_controller.js
+++ app/javascript/controllers/sidemenu-toggle_controller.js
@@ -6,11 +6,26 @@ export default class extends Controller {
       this.toggleTargets.forEach(el => {
         el.classList.add('trigger');
         el.classList.toggle('active');
+        setTimeout(() => {
+          el.classList.add('hidden')
+        }, 200)
+        setTimeout(() => {
+          el.classList.remove('hidden')
+          el.classList.add('show')
+        }, 900)
+        setTimeout(() => {
+          el.classList.toggle('fa-bars')
+          el.classList.toggle('fa-times')
+        }, 1000)
       });
     }
   close() {
     this.toggleTargets.forEach(el => {
       el.classList.remove('active')
+      if (el.classList.contains('fa-times')) {
+        el.classList.remove('fa-times')
+        el.classList.add('fa-bars')
+      }
     });
   }
   }
