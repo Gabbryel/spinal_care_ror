4de90518cdfa4102ecefc48ad40b758a37d12d51
Gabriel Ursache <gabriel@markov.ro>
2022-10-11 16:32:05 +0300

    members error in home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     @review = Review.new()
     @reviews = Review.all.shuffle || []
     @specialties = Specialty.all.order(name: :asc) || []
-    @medics = Profession.find_by(slug: 'medic').members.sample(4) || []
+    @medics = Member.all.where(profession_id: Profession.find_by(name: 'medic')).sample(4)
   end
 
   def medical_team
