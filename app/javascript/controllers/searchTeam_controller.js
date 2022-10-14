12d9f4e494eb7472cd93833bf5c5fc205b5c9623
Gabriel Ursache <gabriel@markov.ro>
2022-10-14 21:18:54 +0300

    attractions on main landing page

--- app/javascript/controllers/searchTeam_controller.js
+++ app/javascript/controllers/searchTeam_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.value = ""
+    this.element.value = "";
   }
   cssController(counter, mainContainer, members) {
     if (counter === 1) {
