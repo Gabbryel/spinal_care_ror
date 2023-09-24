a57683a26acc0c9f2d044e302ef1870eac0c8a43
aquaman <aquaman@MacBook-Pro.local>
2023-09-24 20:59:03 +0300

    added kineto && changed design and form for medical team

--- app/javascript/controllers/searchTeam_controller.js
+++ app/javascript/controllers/searchTeam_controller.js
@@ -4,74 +4,51 @@ export default class extends Controller {
   connect() {
     this.element.value = "";
   }
-  cssController(counter, mainContainer, members) {
-    const validAnswer = () => {
-      let noAnswer = document.getElementById('medical-team__no-answer');
-      if (noAnswer) {
-        mainContainer.removeChild(noAnswer)
-      }
-    }
-    if (counter === 0 ) {
-      if (!document.getElementById('medical-team__no-answer')) {
-        let answer = document.createElement('div');
-        answer.setAttribute('id', 'medical-team__no-answer')
-        let image = "<img src= 'https://res.cloudinary.com/www-spinalcare-ro/image/upload/v1665771386/development/SPINAL_CARE_LOGO_ICON_CC_wulwbr.svg' >"
-        answer.innerHTML = image;
-        mainContainer.appendChild(answer);
-        let answerTxt =  document.createElement('p');
-        answerTxt.innerText = 'Niciun rezultat pentru căutarea dvs.!'
-        answer.appendChild(answerTxt);
-      }
-      mainContainer.style.gridTemplateColumns = '1fr';
-      members.forEach(m => m.style.justifySelf = 'center')
-    } else if (counter === 1) {
-      mainContainer.style.gridTemplateColumns = '1fr';
-      members.forEach(m => m.style.justifySelf = 'center');
-      validAnswer()
-    } else if (counter === 2) {
-      mainContainer.style.gridTemplateColumns = 'repeat(2, minmax(300px, 1fr))';
-      mainContainer.style.maxWidth = '800px';
-      members.forEach(m => m.style.justifySelf = 'center');
-      validAnswer()
-    } else if (counter === 3) {
-      mainContainer.style.gridTemplateColumns = 'repeat(3, minmax(300px, 1fr))';
-      mainContainer.style.maxWidth = '1200px';
-      members.forEach(m => m.style.justifySelf = 'center');
-      validAnswer()
-    } else {
-      mainContainer.style.gridTemplateColumns = 'repeat(auto-fill, minmax(300px, 1fr))';
-      members.forEach(m => m.style.justifySelf = 'unset');
-      mainContainer.style.maxWidth = 'none';
-      validAnswer()
-    }
-  }
+
   filter() {
+    let professionDivs = document.getElementsByClassName('profession-div');
+    this.setProfessionDivsDisplayToGrid(professionDivs)
     let reference = this.element.parentElement.innerText || '';
     let members = Array.from(document.getElementsByClassName('member-card'));
-    let mainContainer = document.getElementById('medical-team');
     let counter = 0;
     members.forEach( m => {
       (m.dataset.profession === reference ||
-       m.dataset.specialty === reference ||
-       reference === 'Toată echipa') ? (m.style.display = 'block', counter++) : (m.style.display = 'none');
-    })
-    this.cssController(counter, mainContainer, members)
-  }
-  search() {
-    let nameReference = this.element.value.toLowerCase();
-    let members = Array.from(document.getElementsByClassName('member-card'));
-    let mainContainer = document.getElementById('medical-team');
-    let counter = 0;
-    members.forEach(m => {
-      if (m.dataset.identity.toLowerCase().includes(nameReference)) {
-        m.style.display = 'block'
-        counter++
+        m.dataset.specialty === reference ||
+        reference === 'Toată echipa') ? (m.style.display = 'block', counter++) : (m.style.display = 'none');
+      })
+      if (reference == 'Toată echipa') {
+        return
       } else {
-        m.style.display = 'none'
+        this.setProfessionDivsDisplayToNone(professionDivs)
+      setTimeout(() => {
+        let mainContainer = document.getElementById('medical-team');
+        this.cssController(counter, mainContainer, members)
+      }, 10)
       }
-    })
-    this.cssController(counter, mainContainer, members)
   }
+
+
+  // search() {
+  //   let professionDivs = document.getElementsByClassName('profession-div');
+  //   this.setProfessionDivsDisplayToGrid(professionDivs)
+  //   let nameReference = this.element.value.toLowerCase();
+  //   let members = Array.from(document.getElementsByClassName('member-card'));
+  //   let counter = 0;
+  //   members.forEach(m => {
+  //     if (m.dataset.identity.toLowerCase().includes(nameReference)) {
+  //       m.style.display = 'block'
+  //       counter++
+  //     } else {
+  //       m.style.display = 'none'
+  //       m.style.height = '0'
+  //     }
+  //   })
+  //   this.setProfessionDivsDisplayToNone(professionDivs)
+  //   setTimeout(() => {
+  //     let mainContainer = document.getElementById('medical-team');
+  //     this.cssController(counter, mainContainer, members)
+  //   }, 100)
+  // }
   
   activateForm() {
     let elementValue = this.element.innerText;
@@ -92,25 +69,83 @@ export default class extends Controller {
       nameForm.style.display = 'block';
     }
   }
-  searchName(event) {
-    if (event.key === 'Enter') {
-      setTimeout(() => {
-        let query = window.location.search;
-        const urlParams  = new URLSearchParams(query);
-        const nameReference = urlParams.get('search[name]')
-        let members = Array.from(document.getElementsByClassName('member-card'));
-        let mainContainer = document.getElementById('medical-team');
-        let counter = 0;
-        members.forEach(m => {
-          if (m.dataset.identity.toLowerCase().includes(nameReference)) {
-            m.style.display = 'block'
-            counter++
-          } else {
-            m.style.display = 'none'
+  // searchName(event) {
+  //   if (event.key === 'Enter') {
+  //     setTimeout(() => {
+  //       let query = window.location.search;
+  //       const urlParams  = new URLSearchParams(query);
+  //       const nameReference = urlParams.get('search[name]')
+  //       let members = Array.from(document.getElementsByClassName('member-card'));
+  //       let mainContainer = document.getElementById('medical-team');
+  //       let counter = 0;
+  //       members.forEach(m => {
+  //         if (m.dataset.identity.toLowerCase().includes(nameReference)) {
+  //           m.style.display = 'block'
+  //           counter++
+  //         } else {
+  //           m.style.display = 'none'
+  //         }
+  //       })
+  //       this.cssController(counter, mainContainer, members)
+  //     }, 500 )
+  //   }
+  // }
+
+  setProfessionDivsDisplayToNone(professionDivs) {
+      Array.from(professionDivs).forEach(pd => {
+        let count = 0
+        Array.from(pd.children).forEach( chd => {
+          if (chd.style.display !== 'none') {
+            count ++
           }
+          return
         })
-        this.cssController(counter, mainContainer, members)
-      }, 500 )
+        if (count === 0) {
+          pd.style.display = 'none'
+          pd.removeAttribute('id', 'profession-div__custom')
+        } else if (count > 0 && count < 4) {
+            pd.style.gridTemplateColumns = `repeat(auto-fill, minmax(300px, 1fr))`
+            pd.style.maxWidth = `${count*400}px`
+            pd.style.justifySelf = 'center'
+        } else {
+          pd.style.gridTemplateColumns = `repeat(auto-fill, minmax(300px, 1fr))`
+        }
+      })
+  }
+
+  setProfessionDivsDisplayToGrid(professionDivs) {
+    Array.from(professionDivs).forEach(pd => {
+      pd.style.display = 'grid'
+      pd.style.gridTemplateColumns = `repeat(auto-fill, minmax(300px, 1fr))`
+      pd.style.maxWidth = 'unset'
+      pd.style.justifySelf = 'unset'
+      Array.from(pd.children).forEach( chd => {
+        chd.style.display = 'unset'
+      })
+    })
+  }
+
+
+
+cssController(counter, mainContainer,  members) {
+  const validAnswer = () => {
+    let noAnswer = document.getElementById('medical-team__no-answer');
+    if (noAnswer) {
+      mainContainer.removeChild(noAnswer)
     }
   }
+  if (counter === 0 ) {
+    if (!document.getElementById('medical-team__no-answer')) {
+      let answer = document.createElement('div');
+      answer.setAttribute('id', 'medical-team__no-answer')
+      // let image = "<img src= 'https://res.cloudinary.com/www-spinalcare-ro/image/upload/v1665771386/development/SPINAL_CARE_LOGO_ICON_CC_wulwbr.svg' >"
+      // answer.innerHTML = image;
+      mainContainer.appendChild(answer);
+      let answerTxt =  document.createElement('p');
+      answerTxt.innerText = 'Niciun rezultat pentru căutarea dvs.!'
+      answer.appendChild(answerTxt);
+    }
+    validAnswer()
+  }
+}
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_10_091448) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_23_173258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_10_091448) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.boolean "has_specialty", default: false
+    t.integer "order"
   end
 
   create_table "reviews", force: :cascade do |t|
