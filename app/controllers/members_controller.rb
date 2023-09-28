61020cfd1926c3195708b3423f869b332bbe8915
aquaman <aquaman@MacBook-Pro.local>
2023-09-28 19:57:08 +0300

    added doctor_grade to members and made it available only for members that are physicians

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -73,6 +73,6 @@ class MembersController < ApplicationController
   end
 
   def member_params
-    params.require(:member).permit(:first_name, :last_name, :profession_id, :specialty_id, :photo, :description, :slug, :academic_title)
+    params.require(:member).permit(:first_name, :last_name, :profession_id, :specialty_id, :photo, :description, :slug, :academic_title, :doctor_grade)
   end
 end
--- app/javascript/controllers/memberFormSpecialty_controller.js
+++ app/javascript/controllers/memberFormSpecialty_controller.js
@@ -1,16 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["profession", "specialty"]
+  static targets = ["profession", "specialty", "grade"]
   connect() {
     let medProf = document.getElementById('med-prof');
     let specialties = this.specialtyTargets;
     let professions = this.professionTargets;
+    let grades = this.gradeTargets;
     professions.forEach((p, i) => {
       let value = Array.from(p.children).find(el => el.value === p.value).innerHTML
       if (medProf.dataset.medicalProfessions.includes(value)) {
         specialties[i].classList.add('trigger')
-        setTimeout(() => {specialties[i].classList.add('active')}, 100)
+        grades[i].classList.add('trigger')
+        setTimeout(() => {
+          specialties[i].classList.add('active')
+          grades[i].classList.add('active')
+        }, 100)
       }
     })
   }
@@ -18,15 +23,24 @@ export default class extends Controller {
     let medProf = document.getElementById('med-prof');
     let specialties = this.specialtyTargets;
     let professions = this.professionTargets;
+    let grades = this.gradeTargets;
     professions.forEach((p, i) => {
       let value = Array.from(p.children).find(el => el.value === p.value).innerHTML
       if (medProf.dataset.medicalProfessions.includes(value)) {
         specialties[i].classList.add('trigger')
-        setTimeout(() => {specialties[i].classList.add('active')}, 100)
+        grades[i].classList.add('trigger')
+        setTimeout(() => {
+          specialties[i].classList.add('active')
+          grades[i].classList.add('active')
+        }, 100)
       }
       else {
         specialties[i].classList.remove('active')
-        setTimeout(() => {specialties[i].classList.remove('trigger')}, 100)
+        grades[i].classList.remove('active')
+        setTimeout(() => {
+          specialties[i].classList.remove('trigger')
+          grades[i].classList.remove('trigger')
+        }, 100)
       }
      }
     )
