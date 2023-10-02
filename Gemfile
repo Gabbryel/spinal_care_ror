f6585ac2e761ac55c303f2293fd668f43f2c2117
aquaman <aquaman@MacBook-Pro.local>
2023-10-02 23:04:32 +0300

    sitemap

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "bootsnap", require: false
 gem "sassc-rails"
 gem 'rack-cors'
 gem 'mini_magick', '~> 4.5', '>= 4.5.1'
+gem 'sitemap_generator'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,10 +1,50 @@
-require 'rubygems'
-require 'sitemap_generator'
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.spinalcare.ro"
+SitemapGenerator::Sitemap.compress = false
 
-SitemapGenerator::Sitemap.default_host = 'https://www.spinalcare.ro'
-SitemapGenerator.verbose = false
 SitemapGenerator::Sitemap.create do
-  add '/', :changefreq => 'daily', :priority => 0.9
-  add '/echipa', :changefreq => 'weekly'
+  add "/",
+  changefreq: 'weekly',
+  priority: 0.4
+
+  add "/echipa",
+  changefreq: 'daily',
+  priority: 0.6
+
+
+  Member.all.order(id: :desc).each do |m|
+    add "/echipa/#{m.slug}",
+    changefreq: 'daily',
+    lastmod: m.updated_at,
+    priority: 0.6
+  end
+
+  Specialty.all.order(id: :desc).each do |s|
+    add "/specialitati-medicale/#{s.slug}",
+    changefreq: 'daily',
+    lastmod: s.updated_at,
+    priority: 0.4
+  end
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
 end
-SitemapGenerator::Sitemap.ping_search_engines
