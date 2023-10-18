058b58e26e2486f7d63575853338bd9a6f1bdd85
aquaman <aquaman@MacBook-Pro.local>
2023-10-18 11:16:22 +0300

    added photos to the side cover

--- app/helpers/bkg_photos_helper.rb
+++ app/helpers/bkg_photos_helper.rb
@@ -4,6 +4,7 @@ module BkgPhotosHelper
       "spinal_care_1_jah9aq.webp",
       "spinal_care_2_wukcoj.webp",
       "spinal_care_3_lv9rat.webp",
+      "spinal_care_4_hbavja.webp",
       "spinal_care_4_hbavja.webp"
     ].sample
   end
