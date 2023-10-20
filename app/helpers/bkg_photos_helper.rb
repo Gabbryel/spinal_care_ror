4a19e6b0a3d751478aad347750113eb27ea1b068
aquaman <aquaman@MacBook-Pro.local>
2023-10-20 10:02:23 +0300

    added ids to links

--- app/helpers/bkg_photos_helper.rb
+++ app/helpers/bkg_photos_helper.rb
@@ -6,7 +6,10 @@ module BkgPhotosHelper
       "spinal_care_3_lv9rat.webp",
       "spinal_care_4_hbavja.webp",
       "spinal_care_4_hbavja.webp",
-      "production/zg9bn5picn9m1th7e5narlkjej8u.webp"
+      "production/zg9bn5picn9m1th7e5narlkjej8u.webp",
+      "side-cover-photos/0171_Large_dtuch2.webp",
+      "side-cover-photos/048_Large_nkswaw.webp",
+      "production/94uullo76kdb478tnw8xr640mq2x.webp"
     ].sample
   end
 end
