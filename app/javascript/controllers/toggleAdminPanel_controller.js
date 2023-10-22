9ca525638e717bfa7866361e5a11827e71523852
aquaman <aquaman@MacBook-Pro.local>
2023-10-22 20:57:52 +0300

    brand new nav bar for ux reason, many old users which didnt find the haburger menu interesiting

--- app/javascript/controllers/toggleAdminPanel_controller.js
+++ app/javascript/controllers/toggleAdminPanel_controller.js
@@ -49,10 +49,12 @@ export default class extends Controller {
       let navbarMenu = document.getElementById('navbar-menu');
       let navbarDistToTop = navbar.getBoundingClientRect().bottom
       if (navbarDistToTop < 73 ) {
+        console.log('working if')
         navbarMenu.style.position = 'fixed';
         navbarMenu.style.width = '100vw';
         navbarMenu.style.top = '0';
       } else if (navbarDistToTop >= 73 ) {
+        console.log('working if else')
         navbarMenu.style.position = 'relative';
         navbarMenu.style.width = 'max-content';
         navbarMenu.style.top = 'unset';
