2fdd8753a5d6a946e8d7cc4b2e21be897101aaf3
aquaman <aquaman@MacBook-Pro.local>
2023-10-22 21:32:59 +0300

    navbar with animation

--- app/javascript/controllers/toggleAdminPanel_controller.js
+++ app/javascript/controllers/toggleAdminPanel_controller.js
@@ -27,6 +27,7 @@ export default class extends Controller {
     }
   }
   connect() {
+    console.log('connect working')
     let body = document.getElementsByTagName("body")[0];
     body.addEventListener("keydown", (e) => {
       if (e.key === "a") {
@@ -45,21 +46,23 @@ export default class extends Controller {
       this.showAdminLink()
     }
   }
+  navbarFixed() {
+    window.onscroll = () => {
+      console.log('window scrolling')
+      let navbar = document.getElementById('navbar');
+      let navbarMenu = document.getElementById('navbar-menu');
+      let navbarDistToTop = navbar.getBoundingClientRect().bottom
+      if (navbarDistToTop < 73 ) {
+        navbarMenu.style.position = 'fixed';
+        navbarMenu.style.width = '100vw';
+        navbarMenu.style.top = '0';
+      } else if (navbarDistToTop >= 73 ) {
+        navbarMenu.style.position = 'relative';
+        navbarMenu.style.width = 'max-content';
+        navbarMenu.style.top = 'unset';
+      }
+    }
+  }
 }
 
-// window.onscroll = () => {
-//   console.log('window scrolling')
-//   let navbar = document.getElementById('navbar');
-//   let navbarMenu = document.getElementById('navbar-menu');
-//   let navbarDistToTop = navbar.getBoundingClientRect().bottom
-//   if (navbarDistToTop < 73 ) {
-//     navbarMenu.style.position = 'fixed';
-//     navbarMenu.style.width = '100vw';
-//     navbarMenu.style.top = '0';
-//   } else if (navbarDistToTop >= 73 ) {
-//     navbarMenu.style.position = 'relative';
-//     navbarMenu.style.width = 'max-content';
-//     navbarMenu.style.top = 'unset';
-//   }
-// }
 
