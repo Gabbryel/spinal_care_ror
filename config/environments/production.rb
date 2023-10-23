0edfacade2a24d8489fcbce29673a837e1444129
aquaman <aquaman@MacBook-Pro.local>
2023-10-23 10:45:18 +0300

    navbar with animation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
 
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   RAILS_ENV=production rake assets:precompile
   config.assets.digest = true
 
