651aa462f8e3d7663a7deced1fb2cbabadbfb0a3
aquaman <aquaman@MacBook-Pro.local>
2023-10-25 10:01:15 +0300

    small changes in fixed bar calc

--- app/assets/builds/application.js
+++ app/assets/builds/application.js
@@ -6497,11 +6497,11 @@
         let navbar = document.getElementById("navbar");
         let navbarMenu = document.getElementById("navbar-menu");
         let navbarDistToTop = navbar.getBoundingClientRect().bottom;
-        if (navbarDistToTop < 73) {
+        if (navbarDistToTop < window.innerHeight * (6 / 100)) {
           navbarMenu.style.position = "fixed";
           navbarMenu.style.width = "100vw";
           navbarMenu.style.top = "0";
-        } else if (navbarDistToTop >= 73) {
+        } else if (navbarDistToTop >= window.innerHeight * (6 / 100)) {
           navbarMenu.style.position = "relative";
           navbarMenu.style.width = "max-content";
           navbarMenu.style.top = "unset";
--- app/javascript/controllers/navbarFixed_controller.js
+++ app/javascript/controllers/navbarFixed_controller.js
@@ -9,11 +9,12 @@ export default class extends Controller {
       let navbar = document.getElementById('navbar');
       let navbarMenu = document.getElementById('navbar-menu');
       let navbarDistToTop = navbar.getBoundingClientRect().bottom
-      if (navbarDistToTop < 73 ) {
+
+      if (navbarDistToTop < window.innerHeight * (6 / 100) ) {
         navbarMenu.style.position = 'fixed';
         navbarMenu.style.width = '100vw';
         navbarMenu.style.top = '0';
-      } else if (navbarDistToTop >= 73 ) {
+      } else if (navbarDistToTop >= window.innerHeight * (6 / 100) ) {
         navbarMenu.style.position = 'relative';
         navbarMenu.style.width = 'max-content';
         navbarMenu.style.top = 'unset';
