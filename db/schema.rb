822fd64ba4009085a6ec76ee07572c23804526bf
Gabbryel <gabriel@markov.ro>
2024-10-17 20:29:16 +0300

    added day hospitalization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_15_180729) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_17_171735) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -130,6 +130,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_15_180729) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.text "description"
+    t.boolean "has_day_hospitalization", default: false
   end
 
   create_table "users", force: :cascade do |t|
