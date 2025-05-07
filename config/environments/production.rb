d95b0c39bf914f22aaa923955fd34cad7c1d63bb
Gabbryel <gabriel@markov.ro>
2025-05-07 14:44:18 +0300

    cloudinary link environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # Store uploaded files on Cloudinary and avoid appending "production" to URLs
   config.active_storage.service = :cloudinary
   config.active_storage.service_urls_expire_in = nil
   # Mount Action Cable outside main process or domain.
